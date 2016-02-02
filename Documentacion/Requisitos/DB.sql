334dd327d584bb67ce71d2c3f4e6a6f3a02135b7
David <davidgarciaromero@gmail.com>
2016-02-02 14:11:11 +0100

    Añadida documentacion sobre los contenidos de la web

---
+++ Documentacion/Requisitos/DB.sql
@@ -0,0 +1,20 @@
+DB
+
+Clientes
+	Nombre y apellidos
+	email
+	telefono
+	password
+	direccion
+	dni
+	
+Pedidos
+	cliente
+	fecha
+	id
+	forma de pago
+	estado
+	
+Productos	
+	
+
---
+++ web/administracion.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>Panel de Administración</title>
+</head>
+
+<body>
+	<header>
+		<nav></nav>
+	</header>
+	<main>
+	
+	</main>
+</body>
+</html>

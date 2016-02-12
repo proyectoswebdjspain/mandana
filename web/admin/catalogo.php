c727f06d2b137e6608486f0df00c688e1a9a5b97
David <davidgarciaromero@gmail.com>
2016-02-12 22:29:58 +0100

    diseño panel definitivo (En construcion visual)

--- web/admin/catalogo.php
+++ web/admin/catalogo.php
@@ -41,12 +41,67 @@
 	// Mostrar y ocultar el formulario de añadir producto	
 		$(document).ready(function(){
 		  $("#ocultar").click(function(){
-			$(".visible").show();
 			$(".oculto").hide();
+			$(".visible").show();
 		  });
 		  $("#mostrar").click(function(){
 			$(".visible").hide();
 			$(".oculto").show();
+			$("#precio").hide();
+			$("#asociados").hide();
+			$("#cantidad").hide();
+			$("#imagenes").hide();
+			$("#proveedores").hide();
+		  });
+		  
+		// muestra y oculta los diferentes campos de catalogo  
+		  $("#boton1").click(function(){
+			$("#informacion").show();
+			$("#precio").hide();
+			$("#asociados").hide();
+			$("#cantidad").hide();
+			$("#imagenes").hide();
+			$("#proveedores").hide();
+		  });
+		  $("#boton2").click(function(){
+			$("#informacion").hide();
+			$("#precio").show();
+			$("#asociados").hide();
+			$("#cantidad").hide();
+			$("#imagenes").hide();
+			$("#proveedores").hide();
+		  });
+		  $("#boton3").click(function(){
+			$("#informacion").hide();
+			$("#precio").hide();
+			$("#asociados").show();
+			$("#cantidad").hide();
+			$("#imagenes").hide();
+			$("#proveedores").hide();
+		  });
+		  $("#boton4").click(function(){
+			$("#informacion").hide();
+			$("#precio").hide();
+			$("#asociados").hide();
+			$("#cantidad").show();
+			$("#imagenes").hide();
+			$("#proveedores").hide();
+		  });
+		  $("#boton5").click(function(){
+			$("#informacion").hide();
+			$("#precio").hide();
+			$("#asociados").hide();
+			$("#cantidad").hide();
+			$("#imagenes").show();
+			$("#proveedores").hide();
+		  });
+		  $("#boton6").click(function(){
+			$("#informacion").hide();
+			$("#precio").hide();
+			$("#asociados").hide();
+			$("#cantidad").hide();
+			$("#imagenes").hide();
+			$("#proveedores").show();
 		  });
 		});
 		
@@ -61,8 +116,8 @@
 	
 	<main>
 		<div class="sub-panel">
-				<h1>Catalogo > Productos <span class="oculto"> > Añadiendo Producto</span></h1>
-				<span class="visible"><a href="#" id="mostrar"><img src="icons/añadir.png" alt="añadir nuevo" title="añadir nuevo"/><p>Añadir nuevo</p></a></span> 
+				<h1>Catalogo > Productos <span class="oculto"> > Añadir Nuevo</span></h1>
+				<span class="visible"><a href="#" id="mostrar"><img src="icons/añadir.png" alt="añadir nuevo" title="añadir nuevo"/><p>Añadir</br>nuevo</p></a></span> 
 				<span class="oculto"> <a href="#" id="ocultar"><img src="icons/cancelar.png" alt="añadir nuevo" title="añadir nuevo"/><p>Cancelar</p> <br/></a></span>
 		</div>	
 		<div class="visible">
@@ -118,91 +173,170 @@
 				</table>
 			</div>
 		</div>
+	<!-- Opciones para añadir un producto nuevo -->	
 		<div class="oculto">
-			<div class="fondo">
-				<form action="catalogo.php" method="POST">
-				<div class="campo-right">
-					<p>Referencia<input type="text" name="producto[6]" size="8" value=""/> </p>
-					<p>Categoria  <select name="producto[3]"> 
-								<?php
-									$sql = "SELECT * FROM categorias";
-									$result = mysqli_query($link, $sql);
-									While($row=mysqli_fetch_array($result)){
-										$categoria = $row['categoria'];
-										$id_categoria = $row['id_categoria'];
-											echo"<option value=\"\">$categoria</option>";
-											$sql2 = "SELECT * FROM sub_categorias WHERE id_categoria = '$id_categoria'";
-											$result2 = mysqli_query($link, $sql2);
-											While($row2=mysqli_fetch_array($result2)){
-												$subcategoria = $row2['sub_categoria'];
-												$id_subcategoria = $row2['id_sub_categoria'];
+			<div class="sub_menu">
+				<ul>
+					<li><a href="#" id="boton1">Información</a></li>
+					<li><a href="#" id="boton2">Precio</a></li>
+					<li><a href="#" id="boton3">Asociaciones</a></li>
+					<li><a href="#" id="boton4">Cantidades</a></li>
+					<li><a href="#" id="boton5">Imagenes</a></li>
+					<li><a href="#" id="boton6">Proveedores</a></li>
+				</ul>
+			</div>
+			<div id="informacion">
+				<div class="fondo">
+					<div id="opcion">
+						<img src="" alt="informacion" /><h2>Informacion</h2>
+					</div>
+					<form action="catalogo.php" method="POST">
+						<div class="campo">
+							<p>Nombre:<input type="text" name="nombre" value=""/> </p>
+						</div>	
+						<div class="campo">
+							<p>Codigo de Referencia:<input type="text" name="referencia" size="8" value=""/> </p>
+						</div>
+						<div class="campo">
+							<p>EAN-13 o Codigo de barra JAN:<input type="text" name="ean" size="13" value=""/> </p>
+						</div>	
+						<div class="campo">
+							<p>Visible:<select name="visible">
+								<option selected value="">--</option>
+								<option  value="1">SI</option>
+								<option  value="0">NO</option>
+								</select> 
+							</p>
+						</div>	
+					
+					
+					
+					
+					
+					<div class="campo">
+						<p>Material:<input type="text" name="producto[13]" value=""/> </p>
+					</div>
+					<div class="campo">
+						<p>Tipo:<input type="text" name="producto[14]" value=""/> </p>
+					</div>
+					<div class="campo">
+						<p>Disponibilidad:<input type="text" name="producto[16]" value=""/> </p>
+					</div>	
+					
+					
+					<div class="detalles">
+						<p>Descripción Corta</p>
+						<textarea name="descripcion_small"> </textarea>
+					</div>
+					<div class="detalles">
+						<p>Descripción	</p>
+						<textarea name="descripcion"> </textarea>
+					</div>
+					<div class="boton">
+						<input type="submit" value="Guardar" name="guardar" />
+					</div>
+					</form>
+				</div>	
+			</div>	
+			<div id="precio">
+				<div class="fondo">
+					<div id="opcion">
+						<img src="" alt="informacion" /><h2>Precio</h2>
+					</div>
+					<form action="catalogo.php" method="POST">
+						
+						<div class="campo">
+							<p>Categoria  <select name="producto[3]"> 
+										<?php
+											$sql = "SELECT * FROM categorias";
+											$result = mysqli_query($link, $sql);
+											While($row=mysqli_fetch_array($result)){
+												$categoria = $row['categoria'];
+												$id_categoria = $row['id_categoria'];
+													echo"<option value=\"\">$categoria</option>";
+													$sql2 = "SELECT * FROM sub_categorias WHERE id_categoria = '$id_categoria'";
+													$result2 = mysqli_query($link, $sql2);
+													While($row2=mysqli_fetch_array($result2)){
+														$subcategoria = $row2['sub_categoria'];
+														$id_subcategoria = $row2['id_sub_categoria'];
+														
+														echo"<option value=\"$id_subcategoria\">--- $subcategoria ($categoria)</option>";
+													}
 												
-												echo"<option value=\"$id_subcategoria\">--- $subcategoria ($categoria)</option>";
 											}
 										
-									}
-								
-								?>
-							</select> </p>
-					<p>Mostrado <select name="producto[11]">
-							<option selected value="">--</option>
-							<option  value="1">SI</option>
-							<option  value="0">NO</option>
-						</select>
-					</p>
-				</div>
-				<div id="imagen">
+										?>
+									</select> </p>
+							<p>Mostrado <select name="producto[11]">
+									<option selected value="">--</option>
+									<option  value="1">SI</option>
+									<option  value="0">NO</option>
+								</select>
+							</p>
+						</div>
+					<div id="imagen">
+						
+					</div>
+					<div id="title">
+						<input type="file" name="foto" />
+					</div>
 					
+					
+					<div class="campo">
+						<p>Precio Base:<input type="text" name="producto[7]" size="5" value=""/> </p>
+					</div>	
+					<div class="campo">
+						<p>IVA:<select name="producto[9]">
+							<?php
+								$sql ="SELECT valor FROM impuestos";
+								$result = mysqli_query($link, $sql);
+								WHILE ($row=mysqli_fetch_array($result) ){
+									$valor = $row['valor'];
+									echo"<option value=\"$valor\">$valor</option>";
+								}
+							?>
+						</select>
+						</p>
+					</div>	
+					<div class="campo">
+						<p>Precio Final:<input type="text" name="producto[8]" size="5" value=""/></p>
+					</div>	
+					<div class="campo">
+						<p>Cantidad:<input type="text" name="producto[10]" value=""/> </p>
+					</div>	
+					<div class="campo">
+						<p>Material:<input type="text" name="producto[13]" value=""/> </p>
+					</div>
+					<div class="campo">
+						<p>Tipo:<input type="text" name="producto[14]" value=""/> </p>
+					</div>
+					<div class="campo">
+						<p>Disponibilidad:<input type="text" name="producto[16]" value=""/> </p>
+					</div>	
+					
+					
+					<div class="detalles">
+						<p>Detalles</p>
+						<textarea name="producto[12]"> </textarea>
+					</div>
+					<div class="boton">
+						<input type="submit" value="Guardar" name="guardar" />
+					</div>
+					</form>
 				</div>
-				<div id="title">
-					<input type="file" name="foto" />
-				</div>
-				
-				<div class="campo">
-					<p>Nombre:<input type="text" name="producto[5]" value=""/> </p>
-				</div>	
-				<div class="campo">
-					<p>Precio Base:<input type="text" name="producto[7]" size="5" value=""/> </p>
-				</div>	
-				<div class="campo">
-					<p>IVA:<select name="producto[9]">
-						<?php
-							$sql ="SELECT valor FROM impuestos";
-							$result = mysqli_query($link, $sql);
-							WHILE ($row=mysqli_fetch_array($result) ){
-								$valor = $row['valor'];
-								echo"<option value=\"$valor\">$valor</option>";
-							}
-						?>
-					</select>
-					</p>
-				</div>	
-				<div class="campo">
-					<p>Precio Final:<input type="text" name="producto[8]" size="5" value=""/></p>
-				</div>	
-				<div class="campo">
-					<p>Cantidad:<input type="text" name="producto[10]" value=""/> </p>
-				</div>	
-				<div class="campo">
-					<p>Material:<input type="text" name="producto[13]" value=""/> </p>
-				</div>
-				<div class="campo">
-					<p>Tipo:<input type="text" name="producto[14]" value=""/> </p>
-				</div>
-				<div class="campo">
-					<p>Disponibilidad:<input type="text" name="producto[16]" value=""/> </p>
-				</div>	
-				
-				
-				<div class="detalles">
-					<p>Detalles</p>
-					<textarea name="producto[12]"> </textarea>
-				</div>
-				<div class="boton">
-					<input type="submit" value="Guardar" name="guardar" />
-				</div>
-				</form>
 			</div>	
+			<div id="asociados">
+			
+			</div>	
+			<div id="cantidad">
+			
+			</div>	
+			<div id="imagenes">
+			
+			</div>	
+			<div id="proveedores">
+			
+			</div>
 		</div>
 	</main>
 </body>
